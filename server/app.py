062287d4f224a7b08eab8f5cfaf935c3938545cf
Diptesh Kumar <dipteshsongara2004@gmail.com>
2026-04-08 23:48:39 +0530

    commit

--- server/app.py
+++ server/app.py
@@ -1,1 +1,15 @@
+import uvicorn
 from server_main import app
+
+
+def main():
+    uvicorn.run(
+        "server_main:app",
+        host="0.0.0.0",
+        port=7860,
+        reload=False
+    )
+
+
+if __name__ == "__main__":
+    main()
